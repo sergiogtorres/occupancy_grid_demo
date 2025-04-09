Work in progress - main.py runs - added real time visualization of scanning ray     - TODO: ^^^ fix mismatch of pixel units vs meter units! in draw_perception_line() and elsewhere

car_lib.py
@@ -1,11 +1,12 @@
 import numpy as np
 import math
+import perception_utils
 class Car():
 
     NOISE_OFF = 0
     NOISE_ON = 1
     def __init__(self, starting_pos, map_x_range, map_y_range, lidar_rps):
-        self.pos = starting_pos
+        self.pos = np.array(starting_pos)
         self.max_range = 10
         self.lidar_dr = 0.10
         self.lidar_dphi = 2*np.pi/100
@@ -18,6 +19,7 @@ class Car():
 
     def update_state(self, dt):
         self.lidar_bearing += dt*self.lidar_bearing_angular_speed
+        self.lidar_bearing = self.lidar_bearing % (2*np.pi)
     def update_relative_range_bearing(self, map_x_range, map_y_range):
         """
         Helper function to get the relative range and bearing meshgrids
@@ -31,17 +33,10 @@ class Car():
 
         self.ground_truth_map_ran = ground_truth_map_ran
         self.ground_truth_map_bearing = ground_truth_map_bearing
-    @staticmethod
-    def check_within_range_bearing(arr_range, arr_bearing, range_down_up, bearing_down_up, ground_truth_map=None):
-        mask = range_down_up[0] <= arr_range
-        mask *= arr_range <= range_down_up[1]
-        mask *= bearing_down_up[0] <= arr_bearing
-        mask *= arr_bearing <= bearing_down_up[1]
-        if ground_truth_map is not None:
-            mask *= ground_truth_map[mask] # now checking if, in those indices, there is an obstacle in the gt map
-        return mask
 
-    def sense(self, bearing, ground_truth_map, noise = NOISE_OFF):
+
+
+    def sense(self, ground_truth_map, noise = NOISE_OFF):
         """
         Uses the ground truth car position and ground truth obstacle map to generate a measurement.
         TODO: include noisy measurements (introduce measurement into bearing -> measure range -> introduce noise to range.
@@ -57,10 +52,13 @@ class Car():
         # 1. loop through the ray to check if obstacle
         for r in np.linspace(0, self.max_range, num = math.ceil(self.max_range/self.lidar_dr)):
             # TODO: before looping, filter the arrays so I only consider the relevant bearings -> index transformations
-            detections = self.check_within_range_bearing(self.ground_truth_map_ran, self.ground_truth_map_bearing,
-                                            [r-self.lidar_dr/2, r+self.lidar_dr/2],
-                                            [bearing-self.lidar_dphi/2, bearing+self.lidar_dphi/2],
-                                                         ground_truth_map)
+            #range_down_up = [r-self.lidar_dr, r+self.lidar_dr]
+            #bearing_down_up = [self.lidar_bearing-self.lidar_dphi, self.lidar_bearing+self.lidar_dphi]
+            detections, _, _ = \
+                perception_utils.check_within_range_bearing(self.ground_truth_map_ran, self.ground_truth_map_bearing,
+                                                            r, self.lidar_bearing, self.lidar_dr, self.lidar_bearing,
+                                                            ground_truth_map,
+                                                            mode=perception_utils.DETECTION_MODE)
 
             if np.any(detections):
                 break

main.py
@@ -1,38 +1,101 @@
 import numpy as np
 import numpy.typing as npt
+import cv2
+from scipy.special import logit, expit
+import time
+
 import car_lib
-from scipy.special import logit
+import perception_utils
 
-def get_meas_bearing(bearing: float) -> float:
-    
-    return ran 
+# TODO: check all instances of use of position -> x, y correct order? cv2 is x, y!
+def draw_perception_line(im, start, range, bearing, dphi):
+    """
+    TODO: adjust to detected range?
+    :param im:
+    :param start:
+    :param range:
+    :param bearing:
+    :param dphi:
+    :return:
+    """
+    pt1 = tuple(start)
+    delta_a = range*np.array([np.cos(bearing+dphi), np.sin(bearing+dphi)])
+    delta_b = range*np.array([np.cos(bearing-dphi), np.sin(bearing-dphi)])
+    x2_a, y2_a = (start + delta_a).astype(np.uint32)
+    x2_b, y2_b = (start + delta_b).astype(np.uint32)
+    pt2_a = x2_a, y2_a
+    pt2_b = x2_b, y2_b
+    #print(f"drawing line from {pt1} to {pt2_a} and {pt2_b}")
+    cv2.line(im, pt1, pt2_a, (0,255,0), 1)
+    cv2.line(im, pt1, pt2_b, (0,255,0), 1)
 
+    cv2.putText(im, f"bearing:{np.round(bearing, 2)}, "
+                    f"range:{np.round(range, 2), }"
+                    f"start:{start}", (50, 30), cv2.FONT_HERSHEY_SIMPLEX,
+                0.5, (0, 255, 0), 1)
 
-def update_belief_map(m_t: npt.NDArray[np.float64], l_prev: npt.NDArray[np.float64], l_0: npt.NDArray[np.float64]) -> float:
+    cv2.putText(im, f"delta_a:{np.round(delta_a, 2)}, ",
+                (50, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
+    cv2.putText(im, f"delta_b:{np.round(delta_b, 2)}, ",
+                (50, 90), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
 
-    pass
-    #return l_t
-
+    cv2.putText(im, f"pt1:{pt1}", (50, 120), cv2.FONT_HERSHEY_SIMPLEX,
+                0.5, (0, 255, 0), 1)
+    cv2.putText(im, f"pt2_a:{pt2_a}", (50, 150), cv2.FONT_HERSHEY_SIMPLEX,
+                0.5, (0, 255, 0), 1)
+    cv2.putText(im, f"pt2_b:{pt2_b}", (50, 180), cv2.FONT_HERSHEY_SIMPLEX,
+                0.5, (0, 255, 0), 1)
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    max_x, max_y =
+    target_fps = 30
+    target_frame_time = 1/target_fps
+    ### Loading obstacle_map ###
+    # Load as grayscale
+    img = cv2.imread('map_big.bmp', cv2.IMREAD_GRAYSCALE)
+    print(img.shape)  # e.g., (height, width)
+    obstacle_map = img == 0
+    lidar_rps = 0.5
+    #############################
+    max_x, max_y = 20, 20 #in meters
     map_x_range = np.linspace(0, max_x, obstacle_map.shape[1])
     map_y_range = np.linspace(0, max_y, obstacle_map.shape[0])
-    car = car_lib.Car()
+    car = car_lib.Car([obstacle_map.shape[1]//2, obstacle_map.shape[0]//2],
+                      map_x_range,map_y_range, lidar_rps)
     running = True
+    l_0 = logit(0.5)
+    l_t = l_prev = np.full(obstacle_map.shape, l_0)
+    dt = target_frame_time
     while running:
+        print(f"dt:{dt}, bearing:{np.round(car.lidar_bearing, 2)}")
+        start_time = time.time()
         # 1. get measurement for bearing
         car.update_relative_range_bearing(map_x_range, map_y_range)
 
-        # bearing = ...
-        # ran = get_meas(bearing)
+        ran = car.sense(ground_truth_map = obstacle_map, noise = car.NOISE_OFF)
 
         # 2. get p(mi| yt)
-
-        # mi = inverse_model(ran, bearing)
+        m_t = perception_utils.inverse_measurement_model(ran, car.lidar_bearing, car)
 
         # 3. update belief map
+        print(l_t)
+        l_prev = np.copy(l_t)
+        l_t = perception_utils.update_belief_map(m_t, l_prev, l_0)
+        grid_frame = cv2.cvtColor((expit(l_t)*255).astype(np.uint8), cv2.COLOR_GRAY2BGR)
+        #grid_frame += np.random.random(grid_frame.shape)
+        draw_perception_line(grid_frame, car.pos, ran, car.lidar_bearing, car.lidar_dphi)
+        cv2.imshow("grid_frame", grid_frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):  # Waits 1ms, breaks on 'q' key
+            break
+        car.update_state(dt) # for now, only rotates the lidar bearing
 
-        # l_t = update_belief_map(m_t, l_prev, l_0)
+        ###### after everything else is finished, calculate required sleep time ######
+        dt = time.time() - start_time
+        sleep_time = target_frame_time - dt
+        if sleep_time > 0:
+            time.sleep(sleep_time)
+            dt = target_frame_time
+
+
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/

perception_utils.py
@@ -1,11 +1,92 @@
 import numpy as np
+import warnings
+from scipy.special import logit
 
+DETECTION_MODE = 0
+INVERSE_MEASUREMENT_MODE = 1
 
+def check_within_range_bearing(arr_range, arr_bearing,
+                               current_range, current_bearing, dr, dphi,
+                               ground_truth_map=None,
+                               mode=DETECTION_MODE):
+    """
+    if ground_truth_map is given, range_down_up, bearing_down_up are teh upper and lower ranges
+    if ground_truth_map is None:
+        checked ranges:  [0, current detection range+dr]
+        checked bearings: [current bearing-dphi, current bearing+dphi]
+
+    TODO: this function does two quite different things:
+        1. simulate the LiDAR detection if mode=DETECTION_MODE
+        2. helper function for inverse_measurement_mode
+        --> separate these into two functions for clarity
+
+    mask_ranges_and_bearings: contains True for any position within the ranges range_down_up, bearing_down_up
+    :param arr_range:
+    :param arr_bearing:
+    :param range_down_up:
+    :param bearing_down_up:
+    :param ground_truth_map:
+    :return:
+        mask_obstacles: contains True for any position where an obstacle is detected (at the end of the ray)
+        mask_no_obstacles: contains True for any position where no obstacle is detected (along the ray)
+                           None if ground_truth_map is given (i.e., working in detection mode)
+        mask_no_info: contains True if no info obtained for each point point
+    """
+    range_down_up = [current_range-dr/2, current_range+dr/2]
+    bearing_down_up = [current_bearing-dphi/2, current_bearing+dphi/2]
+
+    mask_bearings = bearing_down_up[0] <= arr_bearing
+    mask_bearings *= arr_bearing <= bearing_down_up[1]
+
+    mask_ranges = range_down_up[0] <= arr_range
+    mask_ranges *= arr_range <= range_down_up[1]
+    mask_ranges_and_bearings = mask_ranges * mask_bearings
+                                                # now, masks simply contains points within
+                                                # the annular portion defined by the ranges
+
+    # if in measurement mode, check if, in those indices, there is an obstacle in the gt map
+    if ground_truth_map is not None:
+        mask_obstacles = mask_ranges_and_bearings * ground_truth_map
+        mask_no_obstacles = None
+        mask_no_info = None
+
+    else:                                           # no gt map here.
+
+        mask_obstacles = mask_ranges_and_bearings   # we are considering any point within the ranges as a detection
+
+        mask_no_obstacles = (np.bitwise_not(mask_obstacles)) \
+                            & (arr_range < range_down_up[1]) \
+                            & mask_bearings                     # not an obstacle,
+                                                                # closer than the measured distance,
+                                                                # and within the bearing range
+        mask_no_info = np.bitwise_not(mask_obstacles) & np.bitwise_not(mask_no_obstacles)
+
+    if (mode == DETECTION_MODE and ground_truth_map is None) \
+        or (mode == INVERSE_MEASUREMENT_MODE and ground_truth_map is not None):
+        warnings.warn("Wrong combination of mode and ground_truth_map in check_within_range_bearing!")
+
+    return mask_obstacles, mask_no_obstacles, mask_no_info
 def inverse_measurement_model(ran: float, bearing: float, car) -> float:
 
-    m_t = car.check_within_range_bearing(car.ground_truth_map_ran, car.ground_truth_map_bearing,
-                                         [ran-car.lidar_dr/2, ran+car.lidar_dr/2],
-                                         [bearing-car.lidar_dphi/2, bearing+car.lidar_dphi/2],
-                                         ground_truth_map=None)
+    """
+    returns p(m^i|y_t)
+    TODO: implement with Bresenham's line algorithm for Ray Tracing
+    :param ran:
+    :param bearing:
+    :param car:
+    :return:
+    """
+    mask_obstacles, mask_no_obstacles, mask_no_info \
+        = check_within_range_bearing(car.ground_truth_map_ran, car.ground_truth_map_bearing,
+                                     ran, bearing, car.lidar_dr, car.lidar_dphi,
+                                     ground_truth_map=None,
+                                     mode = INVERSE_MEASUREMENT_MODE)
 
+    m_t = mask_obstacles*1 + mask_no_obstacles*0 + mask_no_info*0.5
     return m_t
+
+def update_belief_map(m_t, l_prev, l_0=0.5):
+
+    l_t = logit(m_t) + l_prev - l_0
+
+    return l_t