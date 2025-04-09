b8025104b20f5359257eabff82fd5c373b358d19
Zhahje <sergio.gonzh@gmail.com>
2025-04-09 11:49:34 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+
+def inverse_model(range: float, bearing: float) -> float:
+
+    pass
+
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+
+    # 1. get measurement for bearing
+    # bearing = ...
+    # range = get_meas(bearing)
+
+    # 2. get p(mi| yt)
+
+    # mi = inverse_model(range, bearing)
+
+    # 3. update belief map
+
+    # lt = update_belief_map()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
