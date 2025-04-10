Fixed mismatches of meters and pixel units  - Only major issues in draw_perception_line() and in Car() class in self.pos usage  - Refactored meter/pixel units for clarity  - Refactored functions out of main.py

car_lib.py
@@ -5,16 +5,21 @@ class Car():
 
     NOISE_OFF = 0
     NOISE_ON = 1
-    def __init__(self, starting_pos, map_x_range, map_y_range, lidar_rps):
-        self.pos = np.array(starting_pos)
+    def __init__(self, starting_pos, map_x_range, map_y_range, lidar_rps, pixels_to_a_meter):
+        self.pos_px = np.array(starting_pos)
         self.max_range = 10
         self.lidar_dr = 0.10
         self.lidar_dphi = 2*np.pi/100
         self.lidar_bearing = 0
         self.lidar_bearing_angular_speed = lidar_rps*2*np.pi
+        self.pixels_to_a_meter = pixels_to_a_meter
+
+        ground_truth_map_xx_meters, ground_truth_map_yy_meters = np.meshgrid(map_x_range, map_y_range)
+        self.ground_truth_map_xx_yy_meters = np.dstack([ground_truth_map_xx_meters, ground_truth_map_yy_meters])
 
         self.ground_truth_map_ran = self.ground_truth_map_bearing = None
         self.update_relative_range_bearing(map_x_range, map_y_range)
+        self.delta = None
 
 
     def update_state(self, dt):
@@ -25,11 +30,9 @@ class Car():
         Helper function to get the relative range and bearing meshgrids
         :return:
         """
-        ground_truth_map_XX, ground_truth_map_YY = np.meshgrid(map_x_range, map_y_range)
-        ground_truth_map_XX_YY = np.dstack([ground_truth_map_XX, ground_truth_map_YY])
-        delta = ground_truth_map_XX_YY - self.pos
-        ground_truth_map_ran = np.linalg.norm(delta, axis = 2)
-        ground_truth_map_bearing = np.atan2(delta[:,:,1], delta[:,:,0])
+        self.delta = self.ground_truth_map_xx_yy_meters - self.pos_px / self.pixels_to_a_meter
+        ground_truth_map_ran = np.linalg.norm(self.delta, axis = 2)
+        ground_truth_map_bearing = np.atan2(self.delta[:,:,1], self.delta[:,:,0])
 
         self.ground_truth_map_ran = ground_truth_map_ran
         self.ground_truth_map_bearing = ground_truth_map_bearing
@@ -60,7 +63,8 @@ class Car():
                                                             ground_truth_map,
                                                             mode=perception_utils.DETECTION_MODE)
 
-            if np.any(detections):
+            if np.any(detections) or np.sum(detections)>0:
+                print(f"detection @ r:{np.round(r, 2)}, bearing:{np.round(self.lidar_bearing, 2)}, ")
                 break
 
 

main.py
@@ -8,82 +8,62 @@ import car_lib
 import perception_utils
 
 # TODO: check all instances of use of position -> x, y correct order? cv2 is x, y!
-def draw_perception_line(im, start, range, bearing, dphi):
-    """
-    TODO: adjust to detected range?
-    :param im:
-    :param start:
-    :param range:
-    :param bearing:
-    :param dphi:
-    :return:
-    """
-    pt1 = tuple(start)
-    delta_a = range*np.array([np.cos(bearing+dphi), np.sin(bearing+dphi)])
-    delta_b = range*np.array([np.cos(bearing-dphi), np.sin(bearing-dphi)])
-    x2_a, y2_a = (start + delta_a).astype(np.uint32)
-    x2_b, y2_b = (start + delta_b).astype(np.uint32)
-    pt2_a = x2_a, y2_a
-    pt2_b = x2_b, y2_b
-    #print(f"drawing line from {pt1} to {pt2_a} and {pt2_b}")
-    cv2.line(im, pt1, pt2_a, (0,255,0), 1)
-    cv2.line(im, pt1, pt2_b, (0,255,0), 1)
 
-    cv2.putText(im, f"bearing:{np.round(bearing, 2)}, "
-                    f"range:{np.round(range, 2), }"
-                    f"start:{start}", (50, 30), cv2.FONT_HERSHEY_SIMPLEX,
-                0.5, (0, 255, 0), 1)
-
-    cv2.putText(im, f"delta_a:{np.round(delta_a, 2)}, ",
-                (50, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
-    cv2.putText(im, f"delta_b:{np.round(delta_b, 2)}, ",
-                (50, 90), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
-
-    cv2.putText(im, f"pt1:{pt1}", (50, 120), cv2.FONT_HERSHEY_SIMPLEX,
-                0.5, (0, 255, 0), 1)
-    cv2.putText(im, f"pt2_a:{pt2_a}", (50, 150), cv2.FONT_HERSHEY_SIMPLEX,
-                0.5, (0, 255, 0), 1)
-    cv2.putText(im, f"pt2_b:{pt2_b}", (50, 180), cv2.FONT_HERSHEY_SIMPLEX,
-                0.5, (0, 255, 0), 1)
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     target_fps = 30
     target_frame_time = 1/target_fps
     ### Loading obstacle_map ###
     # Load as grayscale
-    img = cv2.imread('map_big.bmp', cv2.IMREAD_GRAYSCALE)
+    img = cv2.imread('map_always_detect.bmp', cv2.IMREAD_GRAYSCALE)
     print(img.shape)  # e.g., (height, width)
     obstacle_map = img == 0
-    lidar_rps = 0.5
+    lidar_rps = 1/10
     #############################
-    max_x, max_y = 20, 20 #in meters
-    map_x_range = np.linspace(0, max_x, obstacle_map.shape[1])
-    map_y_range = np.linspace(0, max_y, obstacle_map.shape[0])
+    max_x_meters, max_y_meters = 20, 20 #in meters
+
+    pixels_to_a_meter_x = obstacle_map.shape[1]/max_x_meters
+    pixels_to_a_meter_y = obstacle_map.shape[0]/max_y_meters
+
+    norm_difference_ratios = (pixels_to_a_meter_x - pixels_to_a_meter_y)/(pixels_to_a_meter_x + pixels_to_a_meter_y)
+
+    assert norm_difference_ratios < 0.02
+    pixels_to_a_meter = (pixels_to_a_meter_x+pixels_to_a_meter_y)/2
+
+    map_x_range_meters = np.linspace(0, max_x_meters, obstacle_map.shape[1])
+    map_y_range_meters = np.linspace(0, max_y_meters, obstacle_map.shape[0])
     car = car_lib.Car([obstacle_map.shape[1]//2, obstacle_map.shape[0]//2],
-                      map_x_range,map_y_range, lidar_rps)
+                      map_x_range_meters, map_y_range_meters, lidar_rps, pixels_to_a_meter)
     running = True
     l_0 = logit(0.5)
     l_t = l_prev = np.full(obstacle_map.shape, l_0)
     dt = target_frame_time
     while running:
-        print(f"dt:{dt}, bearing:{np.round(car.lidar_bearing, 2)}")
         start_time = time.time()
         # 1. get measurement for bearing
-        car.update_relative_range_bearing(map_x_range, map_y_range)
+        car.update_relative_range_bearing(map_x_range_meters, map_y_range_meters)
 
         ran = car.sense(ground_truth_map = obstacle_map, noise = car.NOISE_OFF)
+        print(f"dt:{dt}, "
+              f"bearing:{np.round(car.lidar_bearing, 2)}, "
+              f"range:{np.round(ran, 2)}")
 
         # 2. get p(mi| yt)
         m_t = perception_utils.inverse_measurement_model(ran, car.lidar_bearing, car)
 
         # 3. update belief map
-        print(l_t)
+        #print(l_t)
         l_prev = np.copy(l_t)
         l_t = perception_utils.update_belief_map(m_t, l_prev, l_0)
         grid_frame = cv2.cvtColor((expit(l_t)*255).astype(np.uint8), cv2.COLOR_GRAY2BGR)
         #grid_frame += np.random.random(grid_frame.shape)
-        draw_perception_line(grid_frame, car.pos, ran, car.lidar_bearing, car.lidar_dphi)
+        perception_utils.draw_perception_line(grid_frame, car.pos_px, ran, car.lidar_bearing, car.lidar_dphi, car.lidar_dr, pixels_to_a_meter)
+        gt_map_draw = np.copy(img)
+        gt_map_draw = cv2.cvtColor((gt_map_draw).astype(np.uint8), cv2.COLOR_GRAY2BGR)
+        perception_utils.draw_perception_line(gt_map_draw, car.pos_px, ran, car.lidar_bearing, car.lidar_dphi, car.lidar_dr, pixels_to_a_meter)
+
         cv2.imshow("grid_frame", grid_frame)
+        cv2.imshow("gt_map", gt_map_draw)
         if cv2.waitKey(1) & 0xFF == ord('q'):  # Waits 1ms, breaks on 'q' key
             break
         car.update_state(dt) # for now, only rotates the lidar bearing

perception_utils.py
@@ -1,10 +1,66 @@
 import numpy as np
 import warnings
 from scipy.special import logit
+import cv2
 
 DETECTION_MODE = 0
 INVERSE_MEASUREMENT_MODE = 1
 
+def draw_perception_line(im, start, range, bearing, dphi, dr, pixels_to_a_meter):
+    """
+    TODO: adjust to detected range?
+    :param im:
+    :param start:
+    :param range:
+    :param bearing:
+    :param dphi:
+    :return:
+    """
+    range_in_pixels = range*pixels_to_a_meter
+    dr_in_pixels = dr*pixels_to_a_meter
+    pt1 = tuple(start)
+    delta_a = range_in_pixels*np.array([np.cos(bearing+dphi), np.sin(bearing+dphi)])
+    delta_b = range_in_pixels*np.array([np.cos(bearing-dphi), np.sin(bearing-dphi)])
+
+    delta_a_3 = dr_in_pixels*np.array([np.cos(bearing+dphi), np.sin(bearing+dphi)])
+    delta_b_3 = dr_in_pixels*np.array([np.cos(bearing-dphi), np.sin(bearing-dphi)])
+
+    x2_a, y2_a = (start + delta_a).astype(np.uint32)
+    x2_b, y2_b = (start + delta_b).astype(np.uint32)
+
+    x3_a, y3_a = (start + delta_a + delta_a_3).astype(np.uint32)
+    x3_b, y3_b = (start + delta_b + delta_b_3).astype(np.uint32)
+
+    pt2_a = x2_a, y2_a
+    pt2_b = x2_b, y2_b
+
+    pt3_a = x3_a, y3_a
+    pt3_b = x3_b, y3_b
+
+
+    #print(f"drawing line from {pt1} to {pt2_a} and {pt2_b}")
+    cv2.line(im, pt1, pt2_a, (0,255,0), 1)
+    cv2.line(im, pt1, pt2_b, (0,255,0), 1)
+
+    cv2.line(im, pt2_a, pt3_a, (0,0,255), 1)
+    cv2.line(im, pt2_b, pt3_b, (0,0,255), 1)
+
+    cv2.putText(im, f"bearing:{np.round(bearing, 2)}, "
+                    f"range:{np.round(range, 2)} m, {np.round(range_in_pixels, 0)} px"
+                    f"start:{start}", (50, 30), cv2.FONT_HERSHEY_SIMPLEX,
+                0.5, (0, 255, 0), 1)
+
+    cv2.putText(im, f"delta_a:{np.round(delta_a, 2)}, ",
+                (50, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
+    cv2.putText(im, f"delta_b:{np.round(delta_b, 2)}, ",
+                (50, 90), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
+
+    cv2.putText(im, f"pt1:{pt1}", (50, 120), cv2.FONT_HERSHEY_SIMPLEX,
+                0.5, (0, 255, 0), 1)
+    cv2.putText(im, f"pt2_a:{pt2_a}", (50, 150), cv2.FONT_HERSHEY_SIMPLEX,
+                0.5, (0, 255, 0), 1)
+    cv2.putText(im, f"pt2_b:{pt2_b}", (50, 180), cv2.FONT_HERSHEY_SIMPLEX,
+                0.5, (0, 255, 0), 1)
 def check_within_range_bearing(arr_range, arr_bearing,
                                current_range, current_bearing, dr, dphi,
                                ground_truth_map=None,
@@ -15,14 +71,15 @@ def check_within_range_bearing(arr_range, arr_bearing,
         checked ranges:  [0, current detection range+dr]
         checked bearings: [current bearing-dphi, current bearing+dphi]
 
+    This function takes in range in meters, since it works with the arr_range array.
     TODO: this function does two quite different things:
         1. simulate the LiDAR detection if mode=DETECTION_MODE
         2. helper function for inverse_measurement_mode
         --> separate these into two functions for clarity
 
     mask_ranges_and_bearings: contains True for any position within the ranges range_down_up, bearing_down_up
-    :param arr_range:
-    :param arr_bearing:
+    :param arr_range: relative range of each point in meters
+    :param arr_bearing: relative bearing of each point in radians
     :param range_down_up:
     :param bearing_down_up:
     :param ground_truth_map:
@@ -65,6 +122,8 @@ def check_within_range_bearing(arr_range, arr_bearing,
         or (mode == INVERSE_MEASUREMENT_MODE and ground_truth_map is not None):
         warnings.warn("Wrong combination of mode and ground_truth_map in check_within_range_bearing!")
 
+    if np.any(mask_obstacles):
+        print(f"obstacle detected!!")
     return mask_obstacles, mask_no_obstacles, mask_no_info
 def inverse_measurement_model(ran: float, bearing: float, car) -> float:
 